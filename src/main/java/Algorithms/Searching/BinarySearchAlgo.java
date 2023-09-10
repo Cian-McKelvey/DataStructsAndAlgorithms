8b6acdf1bacce599db1e4528ec76dced9926c461
Cian-McKelvey <cian.p.mckelvey@gmail.com>
2023-09-10 20:04:12 +0100

    Adding binary search algo

--- src/main/java/Algorithms/Searching/BinarySearchAlgo.java
+++ src/main/java/Algorithms/Searching/BinarySearchAlgo.java
@@ -1,5 +1,46 @@
 package Algorithms.Searching;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class BinarySearchAlgo {
 
+    // array must be pre sorted - x is target value
+    public static int binarySearchAlgo(List<Integer> arr, int x) {
+        int index = - 1; // returns -1 if it cannot be found
+
+        int low = 0;
+        int high = arr.size()-1;
+
+        while (low <= high) {
+
+            int middle = low + (high - low) / 2;
+
+            if (arr.get(middle) == x) return middle;
+
+            if (arr.get(middle) < x) {
+                low = middle + 1;
+            } else {
+                high = middle - 1;
+            }
+        }
+
+        return index;
+    }
+
+    public static void main(String[] args) {
+        List<Integer> testList = new ArrayList<>();
+        testList.add(4);
+        testList.add(9);
+        testList.add(1);
+        testList.add(15);
+        testList.add(3);
+
+        Collections.sort(testList);
+        System.out.println(testList);
+
+        int index = binarySearchAlgo(testList, 9);
+        System.out.println(index);
+    }
 }
